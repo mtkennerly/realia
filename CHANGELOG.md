6121f6f4a0b8c597464cf4438e7278c0da6e26f3
mtkennerly <mtkennerly@gmail.com>
2020-07-12 16:24:03 -0400

    Add doc comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.1.1 (2020-07-12)
+
+* Added doc comments.
+
 ## v0.1.0 (2020-07-11)
 
 * Initial release.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! Realia provides attribute macros for conditional compilation,
+//! analogous to `#[cfg(...)]` and `#[cfg_attr(...)]`.
+
 extern crate proc_macro;
 
 mod attr;
@@ -10,20 +13,100 @@ use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use quote::quote;
 use syn::{parse_macro_input, ItemFn, Result};
 
-macro_rules! res {
-    ($name:ident) => {
-        #[proc_macro_attribute]
-        pub fn $name(args: TokenStream, input: TokenStream) -> TokenStream {
-            cfg(stringify!($name), args, input)
-        }
-    };
+/// Checks whether an environment variable is defined and optionally
+/// what value it has.
+///
+/// If you use this attribute, your project should include a `build.rs` that
+/// triggers a rebuild when any environment variables of interest change:
+///
+/// ```
+/// fn main() {
+///     // Necessary when using #[realia::env("FOO")]
+///     println!("cargo:rerun-if-env-changed=FOO");
+/// }
+/// ```
+///
+/// # Example
+/// ```
+/// #[realia::env("CI")]
+/// fn example() {
+///     println!("CI is defined");
+/// }
+///
+/// #[realia::env("CI", "true")]
+/// fn example() {
+///     println!("CI is set to true");
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn env(args: TokenStream, input: TokenStream) -> TokenStream {
+    cfg("not", args, input)
 }
 
-res!(not);
-res!(any);
-res!(all);
-res!(env);
-res!(cmd);
+/// Checks whether an executable exists on the `PATH`.
+///
+/// If you use this attribute, your project should include a `build.rs` that
+/// triggers a rebuild when the `PATH` environment variable changes:
+///
+/// ```
+/// fn main() {
+///     println!("cargo:rerun-if-env-changed=PATH");
+/// }
+/// ```
+///
+/// # Example
+/// ```
+/// #[realia::cmd("git")]
+/// fn example() {
+///     println!("Git is installed and available");
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn cmd(args: TokenStream, input: TokenStream) -> TokenStream {
+    cfg("not", args, input)
+}
+
+/// Inverts another condition.
+///
+/// # Example
+/// ```
+/// #[realia::not(cmd("git"))]
+/// fn example() {
+///     println!("Git is not installed");
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn not(args: TokenStream, input: TokenStream) -> TokenStream {
+    cfg("not", args, input)
+}
+
+/// Checks if at least one of multiple conditions is met.
+///
+/// # Example
+/// ```
+/// #[realia::any(cmd("git"), cmd("hg"))]
+/// fn example() {
+///     println!("Some version control is available");
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn any(args: TokenStream, input: TokenStream) -> TokenStream {
+    cfg("not", args, input)
+}
+
+/// Checks if multiple conditions are met.
+///
+/// # Example
+/// ```
+/// #[realia::all(cmd("git"), env("GIT_AUTHOR_NAME"))]
+/// fn example() {
+///     println!("Git is available and GIT_AUTHOR_NAME is defined");
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn all(args: TokenStream, input: TokenStream) -> TokenStream {
+    cfg("not", args, input)
+}
 
 fn cfg(top: &str, args: TokenStream, input: TokenStream) -> TokenStream {
     match try_cfg(top, args, input) {
@@ -50,6 +133,20 @@ fn try_cfg(top: &str, args: TokenStream, input: TokenStream) -> Result<TokenStre
     }
 }
 
+/// Applies an attribute when the condition is met.
+/// You can also specify `const` this way.
+///
+/// # Example
+/// ```
+/// #[test]
+/// #[realia::attr(not(cmd("git")), ignore)]
+/// fn some_test_that_requires_git() {}
+/// ```
+///
+/// ```
+/// #[realia::attr(env("USE_CONST_FN"), const)]
+/// fn this_becomes_const() {}
+/// ```
 #[proc_macro_attribute]
 pub fn attr(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as attr::Args);
